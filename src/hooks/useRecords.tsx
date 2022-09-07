b5906005354bd64d6c0faf5b8d2f05229548fbd8
aini <458505413@qq.com>
2022-09-07 11:45:45 +0000

    Update useRecords.tsx and RecordsEdit.tsx

--- src/hooks/useRecords.tsx
+++ src/hooks/useRecords.tsx
@@ -1,56 +1,80 @@
-import {useEffect, useRef, useState} from "react";
-import {createRecordId} from "../lib/createId";
-import day from 'dayjs'
+import { useEffect, useRef, useState } from "react";
+import { createRecordId } from "../lib/createId";
+import day from "dayjs";
 export type RecordItem = {
-    tagId:number
-    note:string
-    category:('+'|'-')
-    amount:number
-    createdAt: string
-    recordId:number
-}
+  tagId: number;
+  note: string;
+  category: "+" | "-";
+  amount: number;
+  createdAt: string;
+  recordId: number;
+};
 type tempRecordItem = {
-    tagId:number
-    note:string
-    category:('+'|'-')
-    amount:number
-}
+  tagId: number;
+  note: string;
+  category: "+" | "-";
+  amount: number;
+};
 export const useRecords = () => {
-    const [records, setRecords] = useState<RecordItem[]>([])
-    const addRecord = (tempRecord:tempRecordItem) => {
-        if(tempRecord.amount <= 0){return}
-        if(tempRecord.tagId === 0){
-            return;
-        }
-        const record = {...tempRecord,recordId:createRecordId(),createdAt:(day(new Date().toISOString())).format('YYYY-MM-DD')}
-        setRecords([...records,record])
+  console.log("useRecords ...");
+  const [records, setRecords] = useState<RecordItem[]>([]);
+  const addRecord = (tempRecord: tempRecordItem) => {
+    if (tempRecord.amount <= 0) {
+      return;
+    }
+    if (tempRecord.tagId === 0) {
+      return;
+    }
+    const record = {
+      ...tempRecord,
+      recordId: createRecordId(),
+      createdAt: day(new Date().toISOString()).format("YYYY-MM-DD"),
     };
-    const findRecord = (recordId:number) => records.filter(record => record.recordId === recordId)[0]
-    const deleteRecord = (id:number) => {
-        setRecords(records.filter(record=>record.recordId !== id))
-    }
-    const updateRecord = (id:number,newCategory:('+'|'-'),newAmount:number,newNote:string) => {
-        setRecords(records.map(record => {
-            if(record.recordId === id){
-                return {...record,
-                    category:newCategory,amount:newAmount,note:newNote
-                };
-            }else {
-                return record;
-            }
-        }))
-    }
-    useEffect(()=>{
-        setRecords(JSON.parse(window.localStorage.getItem('records') || '[]'))
-    },[])
-    const count = useRef(0);
-    useEffect(() => {
-        count.current += 1;
-    })
-    useEffect(()=>{
-        if(count.current > 1){
-            window.localStorage.setItem('records',JSON.stringify(records))
+    setRecords([...records, record]);
+  };
+  const findRecord = (recordId: number) => {
+    console.log("find", recordId);
+    return records.filter((record) => record.recordId === recordId)[0];
+  };
+  const deleteRecord = (id: number) => {
+    setRecords(records.filter((record) => record.recordId !== id));
+  };
+  const updateRecord = (
+    id: number,
+    newCategory: "+" | "-",
+    newAmount: number,
+    newNote: string
+  ) => {
+    setRecords(
+      records.map((record) => {
+        if (record.recordId === id) {
+          return {
+            ...record,
+            category: newCategory,
+            amount: newAmount,
+            note: newNote,
+          };
+        } else {
+          return record;
         }
-    },[records])
-    return{records,addRecord,findRecord,deleteRecord,updateRecord}
-}
+      })
+    );
+  };
+  // useEffect(() => {
+  //   console.log("useEffect []");
+  //   setRecords(JSON.parse(window.localStorage.getItem("records") || "[]"));
+  //   // return;
+  // }, []);
+  const count = useRef(0);
+  useEffect(() => {
+    count.current += 1;
+  });
+  useEffect(() => {
+    console.log("useEffect ...");
+
+    if (count.current > 1) {
+      window.localStorage.setItem("records", JSON.stringify(records));
+    }
+  }, [records]);
+  return { records, addRecord, findRecord, deleteRecord, updateRecord };
+};
--- src/views/RecordsEdit.tsx
+++ src/views/RecordsEdit.tsx
@@ -1,44 +1,44 @@
-import {Link, useParams} from "react-router-dom";
-import React, {useEffect, useLayoutEffect, useState} from "react";
+import { Link, useParams } from "react-router-dom";
+import React, { useEffect, useLayoutEffect, useState } from "react";
 import styled from "styled-components";
-import {TopBar} from "./AddTag";
+import { TopBar } from "./AddTag";
 import Icon from "../components/Icon";
-import {useRecords} from "../hooks/useRecords";
-import {useTags} from "../hooks/useTags";
-import {useDate} from "../hooks/useDate";
-import {AddButton, DeleteButton} from "./PayTagEdit";
+import { useRecords } from "../hooks/useRecords";
+import { useTags } from "../hooks/useTags";
+import { useDate } from "../hooks/useDate";
+import { AddButton, DeleteButton } from "./PayTagEdit";
 type Params = {
-    id:any
-}
+  id: any;
+};
 const RecordBox = styled.div`
   display: flex;
   flex-direction: column;
-  height:100vh;
-  background-color: rgb(254,251,240);
+  height: 100vh;
+  background-color: rgb(254, 251, 240);
   user-select: text;
   -webkit-user-select: text;
-  >header{
+  > header {
     font-weight: bolder;
   }
-`
+`;
 export const CategorySelectBox = styled.ul`
-    display: flex;
-    >li{
-      margin-right: 10px;
-      color: darkgrey;
-    }
-    .selected{
-      color:black;
-      border-bottom: 2px solid rgb(246,50,50);
-    }
-`
+  display: flex;
+  > li {
+    margin-right: 10px;
+    color: darkgrey;
+  }
+  .selected {
+    color: black;
+    border-bottom: 2px solid rgb(246, 50, 50);
+  }
+`;
 const RecordInfoBox = styled.ul`
   display: flex;
   flex-direction: column;
   margin: 10px 20px;
   user-select: text;
   -webkit-user-select: text;
-  >div{
+  > div {
     padding: 5px 0px;
     display: flex;
     border-bottom: 1px solid black;
@@ -48,26 +48,26 @@ const RecordInfoBox = styled.ul`
     overflow: hidden;
     user-select: text;
     -webkit-user-select: text;
-    >span{
+    > span {
       margin-right: 10px;
       margin-left: 10px;
-      >svg{
+      > svg {
         height: 20px;
         width: 20px;
       }
     }
-    >ul{
+    > ul {
       display: flex;
-      >li{
+      > li {
         margin-right: 10px;
         color: darkgrey;
       }
-      .selected{
-        color:black;
-        border-bottom: 1.5px solid rgb(246,50,50);
+      .selected {
+        color: black;
+        border-bottom: 1.5px solid rgb(246, 50, 50);
       }
     }
-    >input{
+    > input {
       background: none;
       user-select: text;
       -webkit-user-select: text;
@@ -77,117 +77,34 @@ const RecordInfoBox = styled.ul`
       margin-right: 10px;
     }
   }
-`
+`;
 const ButtonBox = styled.div`
   margin-top: 100px;
   height: 50px;
   display: flex;
   justify-content: space-evenly;
-  >button{
+  > button {
     width: 30%;
     border: none;
     color: white;
     font-size: 18px;
     font-weight: bolder;
-    &.delete{
-      background-color: rgb(239,112,100);
-      box-shadow: inset -2px -3px 0px rgba(0,0,0,0.25);
+    &.delete {
+      background-color: rgb(239, 112, 100);
+      box-shadow: inset -2px -3px 0px rgba(0, 0, 0, 0.25);
     }
-    &.edit{
-      background-color: rgb(239,167,175);
-      box-shadow: inset -2px -3px 0px rgba(0,0,0,0.25);
+    &.edit {
+      background-color: rgb(239, 167, 175);
+      box-shadow: inset -2px -3px 0px rgba(0, 0, 0, 0.25);
     }
   }
-`
-export const RecordsEdit:React.FC = () => {
-    const {id} = useParams<Params>();
-    const {getName,findTag,IconMap} = useTags();
-    const {findRecord,deleteRecord,updateRecord} = useRecords()
-    const {findDay} = useDate()
-    let record = findRecord(parseInt(id))
-    const [category,setCategory] = useState<('-'|'+')>('-')
-    const [categoryList] = useState<('-'|'+')[]>(['-','+'])
-    const [newAmount,setNewAmount] = useState(0)
-    const [newNote,setNewNote] = useState('')
-    const submit = () => {
-        if(newAmount === 0){
-            window.alert('请输入新的金额')
-            return;
-        }else if(category!=='-'&&category!=='+') {
-            window.alert('请选择一个类别')
-        }else {
-            updateRecord(record.recordId,category,newAmount,newNote)
-            window.alert('修改成功')
-        }
-    }
-    if(record){
-        return(
-        <RecordBox>
-            <TopBar>
-                <Link to='/statistics'><Icon name='return'></Icon></Link>
-                <span></span>
-            </TopBar>
-            <RecordInfoBox>
-                <div>
-                    <span>{getName(record.tagId)}</span><span><Icon name={IconMap[findTag(record.tagId).iconId].name}/></span>
-                </div>
-                <div>
-                    <span>类型</span>
-                    <CategorySelectBox>
-                        {categoryList.map(_category=>
-                            <li key = {_category}
-                                className={_category === category ? 'selected':''}
-                                onClick={()=>setCategory(_category)}
-                            >{_category==='-'?'支出':'收入'}
-                            </li>
-                        )}
-                    </CategorySelectBox>
-                </div>
-                <div>
-                    <span>金额</span>
-                    <input
-                           // onKeyUp="value=value.replace(/^\D*(\d*(?:\.\d{0,2})?).*/g,′1')"
-                            type = 'number'
-                            value={newAmount===0||NaN?record.amount:newAmount}
-                            onChange={(e)=>{
-                                let value = e.target.value.replace(/\D|^0/g, '')
-                                setNewAmount(parseInt(value))
-                                }
-                            }
-                    />
-                </div>
-                <div>
-                    <span>时间</span><span> {findDay(record.createdAt)}</span>
-                </div>
-                <div>
-                    <span>备注</span>
-                    <input
-                        placeholder={record.note===''?'输入备注':record.note}
-                        onChange={(e)=> {setNewNote(e.target.value)
-                        }}
-                    />
-                </div>
-            </RecordInfoBox>
-            <ButtonBox>
-                <AddButton
-                    // onClick={()=>updateRecord(record.recordId,category,newAmount,newNote)}
-                    onClick={()=>submit()}
-                >
-                    确认修改
-                </AddButton>
-                <DeleteButton
-                    onClick={()=>{deleteRecord(record.recordId);window.location.assign('./')}}
-                >
-                    删除记录
-                </DeleteButton>
-            </ButtonBox>
-        </RecordBox>
-    )
-    }else{
-        return (
-            <div>
-                404 NOT FOUND
-            </div>
-        )
-    }
-}
+`;
+export const RecordsEdit: React.FC = () => {
+  //   const { id } = useParams<Params>();
+  //   const { getName, findTag, IconMap } = useTags();
+  const { findRecord, deleteRecord, updateRecord } = useRecords();
+  //   const { findDay } = useDate();
+  let record = findRecord(1);
+  console.log("aaaaa");
+  return <div>404 NOT FOUND</div>;
+};
